59051fd69a6165efea0dc41199f21e6abab6a79b
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2023-09-01 09:50:51 -0400

    added json database

--- react-prac/src/App.js
+++ react-prac/src/App.js
@@ -6,13 +6,7 @@ import Persons from "./components/Persons";
 import axios from "axios";
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const addPerson = (newName, newNumber) => {
     if (newName.trim() !== '') {
@@ -34,7 +28,7 @@ function App() {
   
     const eventHandler = response => {
       console.log('promise fulfilled')
-      setNotes(response.data)
+      setPersons(response.data)
     }
   
     const promise = axios.get('http://localhost:3001/persons')
