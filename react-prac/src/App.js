02ef185064665d1d4a2da7504e4024f883fb59c8
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2023-09-01 07:39:27 -0400

    te

--- react-prac/src/App.js
+++ react-prac/src/App.js
@@ -1,24 +1,38 @@
 import { useState } from "react";
 
 function App() {
-  // States
+  // List of people in DB
+
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
-  const [newName, setNewName] = useState('')
+  // States
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   // Event for when a new person is added and using preventDefault on the addedPerson so that the page doesn't reload.
   const addPerson = (e) => {
     e.preventDefault();
     
     if (newName.trim() !== '') {
-      // Check if the name is already in the list
+      // Check if the name or number is already in the list
+
       const nameExists = persons.some(person => person.name === newName);
-      
+      const numberExists = persons.some(person => person.number === newNumber);
+
       if (nameExists) {
         alert(`${newName} is already in the phonebook.`);
+
+      } else if (numberExists) {
+        alert(`${newNumber} is already in the phonebook.`);
+
       } else {
-        setPersons([...persons, { name: newName }]);
+        setPersons([...persons, { name: newName, number: newNumber }]);
+        setNewNumber('');
         setNewName('');
       }
     }
@@ -31,16 +45,34 @@ function App() {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input type='text' value={newName} onChange={(e) => setNewName(e.target.value)}/>
-        </div>
-        <div>
-          <button type="submit" onClick={addPerson}>add</button>
+          filter: <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
         </div>
       </form>
+      <div>
+        <h2>Add New Person</h2>
+        <form>
+          <div>
+            name: <input type='text' value={newName} onChange={(e) => setNewName(e.target.value)}/>
+          </div>
+          <div>
+            number: <input type="text" value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+          </div>
+          <div>
+            <button type="submit" onClick={addPerson}>add</button>
+          </div>
+        </form>
+      </div>
+
       <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <p key={i}>{person.name}</p>
-      ))}
+      {persons
+        .filter(person =>
+          person.name.toLowerCase().includes(filter.toLowerCase()) ||
+          person.number.includes(filter)
+        )
+        .map((person) => (
+           <p key={person.id}>{person.name}: {person.number}</p>
+        ))
+        }
     </div>
   );
 }
